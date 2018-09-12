a5c515d3c51b2fbd0d757d19f00da17487226bee
Robert Kloefkorn <robert.kloefkorn@iris.no>
2018-09-12 15:43:28 +0200

    Adjustment for AMGX

--- examples/flow_amgxpetsc.cpp
+++ examples/flow_amgxpetsc.cpp
@@ -1,6 +1,7 @@
 // only compile blackoil option
 #define FLOW_SINGLE_PURPOSE 1
 #define ENABLE_FLOW_BLACKOIL 1
-// petsc solvers
+// choose solvers
+#define USE_AMGX_SOLVERS 1
 #define FLOW_USE_DUNE_FEM_PETSC 1
 #include "flow.cpp"
